e6de6d484d6ec59e683da73be41c534dd389bbcc
emrullahakturk <akturk.emrullah@hotmail.com>
2024-08-02 19:37:25 +0300

    User Past Birthdays Layout and Fragment Added. Navigations Adjusted

--- app/src/main/java/com/yargisoft/birthify/views/auth_user_views/PastBirthdaysFragment.kt
+++ app/src/main/java/com/yargisoft/birthify/views/auth_user_views/PastBirthdaysFragment.kt
@@ -1,60 +1,141 @@
 package com.yargisoft.birthify.views.auth_user_views
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.yargisoft.birthify.R
+import com.yargisoft.birthify.UserFrequentlyUsedFunctions
+import com.yargisoft.birthify.adapters.BirthdayAdapter
+import com.yargisoft.birthify.databinding.FragmentAuthMainPageBinding
+import com.yargisoft.birthify.databinding.FragmentPastBirthdaysBinding
+import com.yargisoft.birthify.repositories.AuthRepository
+import com.yargisoft.birthify.repositories.BirthdayRepository
+import com.yargisoft.birthify.sharedpreferences.UserSharedPreferencesManager
+import com.yargisoft.birthify.viewmodels.AuthViewModel
+import com.yargisoft.birthify.viewmodels.UsersBirthdayViewModel
+import com.yargisoft.birthify.viewmodels.factories.AuthViewModelFactory
+import com.yargisoft.birthify.viewmodels.factories.UsersBirthdayViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PastBirthdaysFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PastBirthdaysFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentPastBirthdaysBinding
+    private lateinit var usersBirthdayViewModel: UsersBirthdayViewModel
+    private lateinit var authViewModel: AuthViewModel
+    private lateinit var userSharedPreferences: UserSharedPreferencesManager
+    private lateinit var adapter: BirthdayAdapter
+    private lateinit var itemTouchHelper: ItemTouchHelper
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_past_birthdays, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PastBirthdaysFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PastBirthdaysFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_past_birthdays, container, false)
+
+        //Birthday viewModel Tanımlama için gerekenler
+        val birthdayRepository = BirthdayRepository(requireContext())
+        val birthdayFactory = UsersBirthdayViewModelFactory(birthdayRepository)
+        usersBirthdayViewModel = ViewModelProvider(this, birthdayFactory)[UsersBirthdayViewModel::class]
+
+
+        //Auth ViewModel Tanımlama için gerekenler
+        val authRepository = AuthRepository(requireContext())
+        val authFactory = AuthViewModelFactory(authRepository)
+        authViewModel = ViewModelProvider(this, authFactory)[AuthViewModel::class]
+
+
+
+        //doğum günlerini liveDataya çekiyoruz
+        usersBirthdayViewModel.getPastBirthdaysFromFirebase(authViewModel.loggedUserId())
+        usersBirthdayViewModel.getPastBirthdays()
+
+        //user SharedPreferences
+        userSharedPreferences = UserSharedPreferencesManager(requireContext())
+
+
+
+        // DrawerLayout ve NavigationView tanımlamaları
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navigationView: NavigationView = binding.navigationView
+        val toolbarMenuButton = binding.toolbarPastBirthdays.findViewById<View>(R.id.menuButtonToolbar)
+
+        //Navigation View'i açıp kapamaya ve menü içindeki elemanlarla başka sayfalara gitmemizi sağlayan fonksiyon
+        UserFrequentlyUsedFunctions.drawerLayoutToggle(
+            drawerLayout,
+            navigationView,
+            findNavController(),
+            toolbarMenuButton,
+            requireActivity(),
+            authViewModel,
+            birthdayRepository,
+            userSharedPreferences,
+            "PastBirthdays"
+        )
+
+        val adapterList= usersBirthdayViewModel.pastBirthdayList.value ?: emptyList()
+
+        //Adapter'ı initialize etme
+        adapter = BirthdayAdapter(
+            adapterList.sortedByDescending { it.recordedDate },
+            {birthday ->
+                val action =MainPageFragmentDirections.mainToEditBirthday(birthday)
+                findNavController().navigate(action)
+            },
+            { birthday ->
+                val action = MainPageFragmentDirections.mainToDetailBirthday(birthday)
+                findNavController().navigate(action)
+            },
+            requireContext(),
+            binding.clickToAddBirthdayTv)
+
+
+        //Doğum günlerini viewmodel içindeki live datadan observe ederek ekrana yansıtıyoruz
+        usersBirthdayViewModel.pastBirthdayList.observe(viewLifecycleOwner) { birthdays ->
+            adapter.updateData(birthdays)
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+
+        //Search edittext'i ile doğum günü arama ekliyoruz
+        binding.searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                UserFrequentlyUsedFunctions.filterBirthdays(s.toString(), usersBirthdayViewModel, adapter)
             }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.toolbarPastBirthdays.findViewById<View>(R.id.addButtonToolbar).setOnClickListener {
+            findNavController().navigate(R.id.mainToAddBirthday)
+        }
+        binding.bottomNavigationView.findViewById<View>(R.id.bottomNavBirthdays)
+            .setOnClickListener {
+                it.findNavController().navigate(R.id.mainToMain)
+            }
+
+        binding.sortButton.setOnClickListener {
+            UserFrequentlyUsedFunctions.showSortMenu(it, requireContext(), adapter, usersBirthdayViewModel)
+        }
+
+
+        return  binding.root
     }
 }
