a0781800615e5bbcd0ad8690b5e56e483b3dffd2
emrullahakturk <akturk.emrullah@hotmail.com>
2024-08-15 21:41:20 +0300

    User And Guest Settings UI Added

--- app/src/main/java/com/yargisoft/birthify/views/auth_user_views/SettingsFragment.kt
+++ app/src/main/java/com/yargisoft/birthify/views/auth_user_views/SettingsFragment.kt
@@ -15,7 +15,7 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_auth_settings, container, false)
 
 
 
