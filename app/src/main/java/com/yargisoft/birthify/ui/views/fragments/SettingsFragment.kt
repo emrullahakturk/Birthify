92ac98cc7000e835bcb1fa1134ca1169e92d41f1
unknown <akturk.emrullah@hotmail.com>
2024-12-14 01:40:37 +0300

    Settings Fragment Modified

--- app/src/main/java/com/yargisoft/birthify/ui/views/fragments/SettingsFragment.kt
+++ app/src/main/java/com/yargisoft/birthify/ui/views/fragments/SettingsFragment.kt
@@ -38,25 +38,122 @@ import javax.inject.Inject
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentAuthSettingsBinding
-
     private val authViewModel: AuthViewModel by viewModels()
 
     @Inject
     lateinit var userSharedPreferences: UserSharedPreferencesManager
+
     @Inject
     lateinit var birthdayRepository: BirthdayRepository
 
-
-    companion object{
+    companion object {
         private const val PREFS_NAME = "AppSettings"
         private const val LANGUAGE_KEY = "AppLanguage"
     }
 
-
-    // İzin isteme işlemini başlatmak için launcher
     private val requestPermissionLauncher = registerForActivityResult(
-    ActivityResultContracts.RequestPermission()
+        ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
+        handleNotificationPermissionResult(isGranted)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_auth_settings, container, false)
+
+        initializeUI()
+        setupListeners()
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateUIState()
+    }
+
+    // Kullanıcı Arayüzü Bileşenlerini Başlatma
+    private fun initializeUI() {
+        updateNotificationSwitch()
+        binding.darkThemeSwitch.isChecked = userSharedPreferences.isDarkThemeEnabled()
+    }
+
+    // Kullanıcı Etkileşimlerini Tanımlama
+    private fun setupListeners() {
+        setupNotificationSwitch()
+        setupDarkThemeSwitch()
+        setupLanguageSelection()
+        setupCardViewListeners()
+        setupLogoutListener()
+        setupBackButtonListener()
+    }
+
+    // Bildirim Switch'i Yönetimi
+    private fun setupNotificationSwitch() {
+        binding.notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) enableNotifications() else disableNotifications()
+        }
+    }
+
+    // Karanlık Tema Switch'i Yönetimi
+    private fun setupDarkThemeSwitch() {
+        binding.darkThemeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            userSharedPreferences.setDarkThemeToggle(isChecked)
+            if (isChecked) enableDarkTheme() else disableDarkTheme()
+        }
+    }
+
+    // Dil Seçimi
+    private fun setupLanguageSelection() {
+        val currentLanguage = getCurrentLanguage()
+        binding.languageSelectImageView.setOnClickListener {
+            showLanguageSelectionDialog(currentLanguage)
+        }
+    }
+
+    // Kart Görünümü Etkileşimlerini Tanımlama
+    private fun setupCardViewListeners() {
+        binding.whatIsBirthifyCardView.setOnClickListener {
+            WhatIsBirthifyDialogFragment().show(parentFragmentManager, "WhatIsBirthifyDialog")
+        }
+
+        binding.faqCardView.setOnClickListener {
+            com.yargisoft.birthify.ui.views.dialogs.FrequentlyAskedQuestionsDialogFragment()
+                .show(parentFragmentManager, "FrequentlyAskedQuestionsDialog")
+        }
+
+        binding.contactUsCardView.setOnClickListener { sendSupportEmail() }
+
+        binding.privacyPolicyCardView.setOnClickListener {
+            PrivacyPolicyDialogFragment().show(parentFragmentManager, "PrivacyPolicyDialog")
+        }
+    }
+
+    // Çıkış İşlemini Tanımlama
+    private fun setupLogoutListener() {
+        binding.logoutCardView.setOnClickListener {
+            performLogout()
+        }
+    }
+
+    // Geri Butonunu Tanımlama
+    private fun setupBackButtonListener() {
+        binding.fabBackButtonDetail.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    // Çıkış İşlemi
+    private fun performLogout() {
+        authViewModel.logoutUser()
+        userSharedPreferences.clearUserSession()
+        birthdayRepository.clearAllBirthdays()
+        logout(requireActivity())
+    }
+
+    // Bildirim İzni Sonucunu Yönetme
+    private fun handleNotificationPermissionResult(isGranted: Boolean) {
         if (isGranted) {
             Log.d("NotificationPermission", "Bildirim izni verildi.")
         } else {
@@ -64,135 +161,41 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_auth_settings, container, false)
-
-
-
-        // Bildirim izinlerini kontrol et ve Switch'i ayarla
-        updateNotificationSwitch()
-
-
-        // Karanlık tema ayarını kontrol et ve Switch'i ayarla
-        val isDarkThemeEnabled = userSharedPreferences.isDarkThemeEnabled()
-        binding.darkThemeSwitch.isChecked = isDarkThemeEnabled
-
-
-
-
-        // Bildirim Switch'ini yönet
-        binding.notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                enableNotifications()
-            } else {
-                disableNotifications()
-            }
-        }
-
-        // Karanlık Tema Switch'ini yönet
-        binding.darkThemeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            userSharedPreferences.setDarkThemeToggle(isChecked)
-            if (isChecked) {
-                enableDarkTheme()
-            } else {
-                disableDarkTheme()
-            }
-        }
-
-
-        val currentLanguage = getCurrentLanguage()
-        binding.languageSelectImageView.setOnClickListener {
-            showLanguageSelectionDialog(currentLanguage)
-        }
-
-        binding.whatIsBirthifyCardView.setOnClickListener {
-            val dialogFragment = WhatIsBirthifyDialogFragment()
-            dialogFragment.show(parentFragmentManager, "WhatIsBirthifyDialog")
-        }
-
-        binding.faqCardView.setOnClickListener {
-            val dialogFragment =
-                com.yargisoft.birthify.ui.views.dialogs.FrequentlyAskedQuestionsDialogFragment()
-            dialogFragment.show(parentFragmentManager, "FrequentlyAskedQuestionsDialog")
-        }
-
-        binding.contactUsCardView.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:")
-                putExtra(Intent.EXTRA_EMAIL, arrayOf("support@birthify.com"))
-                putExtra(Intent.EXTRA_SUBJECT, "Support Request")
-                putExtra(Intent.EXTRA_TEXT, "Hello, I need help with...")
-            }
-
-            if (intent.resolveActivity(requireActivity().packageManager) != null) {
-                startActivity(intent)
-            } else {
-                Toast.makeText(requireContext(),getString(R.string.support_email_snackbar), Toast.LENGTH_SHORT).show()
-            }
-        }
-
-
-        binding.privacyPolicyCardView.setOnClickListener {
-            val dialogFragment = PrivacyPolicyDialogFragment()
-            dialogFragment.show(parentFragmentManager, "PrivacyPolicyDialog")
-        }
-
-
-        binding.logoutCardView.setOnClickListener {
-            authViewModel.logoutUser()
-            userSharedPreferences.clearUserSession()
-            birthdayRepository.clearAllBirthdays()
-            logout(requireActivity())
-        }
-        binding.fabBackButtonDetail.setOnClickListener {
-            findNavController().popBackStack()
-        }
-
-
-        return binding.root
-    }
-
+    // Mevcut Dil Seçimini Alma
     private fun getCurrentLanguage(): String {
         val preferences = requireContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         return preferences.getString(LANGUAGE_KEY, Locale.getDefault().language) ?: Locale.getDefault().language
     }
 
+    // Dil Seçim Diyaloğunu Gösterme
     private fun showLanguageSelectionDialog(currentLanguage: String) {
-        val languages = arrayOf("English", "Türkçe") // Dil seçenekleri
-        val languageCodes = arrayOf("en", "tr") // Dil kodları
+        val languages = arrayOf("English", "Türkçe")
+        val languageCodes = arrayOf("en", "tr")
 
         AlertDialog.Builder(requireContext())
             .setTitle(getString(R.string.select_language_title))
             .setItems(languages) { _, which ->
                 val selectedLanguage = languageCodes[which]
-                if (selectedLanguage != currentLanguage) {
-                    setLocale(selectedLanguage)
-                }
+                if (selectedLanguage != currentLanguage) setLocale(selectedLanguage)
             }
             .show()
     }
 
+    // Uygulama Dilini Ayarlama
     private fun setLocale(languageCode: String) {
         val preferences = requireContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         preferences.edit().putString(LANGUAGE_KEY, languageCode).apply()
 
-        // Uygulamanın dilini değiştir ve Activity'yi yeniden başlat
         val intent = Intent(requireContext(), MainActivity::class.java)
         startActivity(intent)
         requireActivity().finish()
     }
 
-
+    // Bildirimleri Etkinleştirme
     private fun enableNotifications() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            // Tiramisu ve sonrasında bildirim izni iste
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         } else {
-            // Tiramisu öncesi doğrudan bildirimleri etkinleştir
             val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             if (!notificationManager.areNotificationsEnabled()) {
                 val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
@@ -202,50 +205,56 @@ class SettingsFragment : Fragment() {
         }
     }
 
+    // Bildirimleri Devre Dışı Bırakma
     private fun disableNotifications() {
         val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
             .putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
         startActivity(intent)
     }
 
-
+    // Bildirim Durumunu Güncelleme
     private fun updateNotificationSwitch() {
         val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val isNotificationEnabled = notificationManager.areNotificationsEnabled()
-        binding.notificationSwitch.isChecked = isNotificationEnabled
+        binding.notificationSwitch.isChecked = notificationManager.areNotificationsEnabled()
     }
 
+    // Karanlık Temayı Etkinleştirme
     private fun enableDarkTheme() {
-        // Karanlık temayı etkinleştir
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
+    // Karanlık Temayı Devre Dışı Bırakma
     private fun disableDarkTheme() {
-        // Karanlık temayı devre dışı bırak
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        // Switch durumlarını güncelle
-        updateNotificationSwitch()
-
-
-        val isDarkThemeEnabled = userSharedPreferences.isDarkThemeEnabled()
-        binding.darkThemeSwitch.isChecked = isDarkThemeEnabled
-    }
-
-    // Logout fonksiyonu
+    // Oturum Kapatma
     private fun logout(activity: Activity) {
-        // Mevcut aktiviteyi kapatır ve yeni bir aktivite başlatır yani uygulama sıfırdan başlamış gibi olur
         val intent = Intent(activity, AuthActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         activity.startActivity(intent)
         activity.finish()
     }
 
+    // Destek E-postası Gönderme
+    private fun sendSupportEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("support@birthify.com"))
+            putExtra(Intent.EXTRA_SUBJECT, "Support Request")
+            putExtra(Intent.EXTRA_TEXT, "Hello, I need help with...")
+        }
 
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.support_email_snackbar), Toast.LENGTH_SHORT).show()
+        }
+    }
 
-
+    // Kullanıcı Arayüzü Durumunu Güncelleme
+    private fun updateUIState() {
+        updateNotificationSwitch()
+        binding.darkThemeSwitch.isChecked = userSharedPreferences.isDarkThemeEnabled()
+    }
 }
